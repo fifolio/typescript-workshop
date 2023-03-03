c622e1a2903bcc64b11f0f8cf8a9cfac0412db3a
Firas <firasdabbabi@gmail.com>
2023-03-03 18:32:58 +0100

    version 1.1, types

---
+++ basic1.js
@@ -0,0 +1,27 @@
+"use strict";
+console.clear();
+// any
+let o = 5;
+o = "f";
+// console.log(o);
+// array
+let v = [1, 2, 3];
+// console.log(v);
+let s = ["this", "is", "string"];
+// console.log(s);
+let z = [1, "address", {}, true];
+// console.log(z);
+// null
+let n = null;
+// console.log(n);
+// undefined
+let u = undefined;
+// console.log(u);
+// String
+let a = "Hello";
+let b = "world";
+// console.log(a + b);
+// Numbers
+let x = 2;
+let y = 3;
+// console.log(x + y);
---
+++ basic1.ts
@@ -0,0 +1,32 @@
+console.clear();
+
+// any
+let o: any = 5;
+o = "f";
+// console.log(o);
+
+// array
+let v: number[] = [1, 2, 3];
+// console.log(v);
+let s: string[] = ["this", "is", "string"];
+// console.log(s);
+let z: [number, string, object, boolean] = [1, "address", {}, true];
+// console.log(z);
+
+// null
+let n: null = null;
+// console.log(n);
+
+// undefined
+let u: undefined = undefined;
+// console.log(u);
+
+// String
+let a: string = "Hello";
+let b: string = "world";
+// console.log(a + b);
+
+// Numbers
+let x: number = 2;
+let y: number = 3;
+// console.log(x + y);
